05166cb5dbf1f5e81a7fb14320c9fa14614a9150
TVVinudev <vinudevtv@gmail.com>
2025-02-25 12:04:24 +0530

    server side

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    
+    @Get()
+    findAll(){
+        return []
+    }
+
+    @Get(':id')
+    findOne(@Param('id')id:string){
+        return {id}
+    }
+
+    @Post() 
+    create(@Body() user:{}){
+        return user
+    }
+
+    @Patch(':id')
+    updateOne(@Param('id')id:string, @Body() userUpdate: {} ){
+        return {id, ...userUpdate }
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id')id:string){
+        return {id}
+    }
+
+}
